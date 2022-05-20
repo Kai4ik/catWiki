0db0417a0167f792541b9f2855a81a156c18f302
Kairat <orozobekov.kai@gmail.com>
2022-05-20 02:48:25 -0400

    Fonts added

--- src/html.js
+++ src/html.js
@@ -6,7 +6,11 @@ export default function HTML(props) {
     <html {...props.htmlAttributes}>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Mystery+Quest&display=swap"
           rel="stylesheet"
