7c3609fa71c40fa3b1f0784e698b0d0e6328ce55
Kairat <orozobekov.kai@gmail.com>
2022-05-21 00:41:39 -0400

    Resolve issue with header which is not displayed on production

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,14 @@ import { StaticImage } from "gatsby-plugin-image";
 
 const Header = () => {
   return (
-    <StaticImage
-      src="../images/CatwikiLogo.svg"
-      alt="catWiki logo"
-      objectFit="contain"
-      objectPosition="left"
-    />
+    <header>
+      <StaticImage
+        src="../images/CatwikiLogo.svg"
+        alt="catWiki logo"
+        objectFit="contain"
+        objectPosition="left"
+      />
+    </header>
   );
 };
 
